aa164b1ac8d323c480facd2fadf0079758d3c245
tony-go <gorez.tony@gmail.com>
2021-11-28 13:40:51 +0100

    chore: move code into a few component

---
+++ components/Layout.tsx
@@ -0,0 +1,15 @@
+import { ReactChild } from "react";
+
+interface Props {
+  children: ReactChild
+}
+
+export default function Layout ({ children }: Props) {
+  return (
+    <div className="flex min-h-screen md:p-20 bg-gray-800">
+      <div className="container m-auto">
+        {children}
+      </div>
+    </div>
+  )
+}
---
+++ utils/ScannerStorage.ts
@@ -0,0 +1,12 @@
+export class ScannerStorage {
+  private prefix: string;
+
+  constructor(prefix = "scanner") {
+    this.prefix = prefix;
+  }
+
+  set() {}
+  get() {}
+  remove() {}
+  clear() {}
+}
