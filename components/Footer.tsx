787d580114971c5b6fbbcb759bfa4874a258b66f
MehdiHan <meh.bouchard@outlook.fr>
2021-12-30 10:32:15 +0100

    feat(ui): Adding Footer Component and NodeSecure Logo

---
+++ components/Footer.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export default function Footer() {
+  return (
+    <footer className="flex items-center">
+      <Image
+        className="rounded-full"
+        src="https://avatars.githubusercontent.com/u/85318671?s=60&v=4"
+        alt="NodeSecure Logo"
+        width={40}
+        height={40}
+      />
+      <p className="ml-3 text-gray-200 font-thin font-sans">
+        Powered by NodeSecure -{" "}
+        <a
+          className="hover:text-gray-400"
+          href="https://github.com/ES-Community/nsecure"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Link to the organization
+        </a>
+      </p>
+    </footer>
+  );
+}
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import { ReactChild } from "react";
+import Footer from "./Footer";
 
 interface Props {
   children: ReactChild;
@@ -8,19 +9,7 @@ export default function Layout({ children }: Props) {
   return (
     <div className="flex flex-col items-center min-h-screen md:p-20 bg-gray-800">
       <div className="container m-auto">{children}</div>
-      <footer>
-        <p className="text-gray-200 font-thin font-sans">
-          🕸 Powered by NodeSecure -{" "}
-          <a
-            className="hover:text-gray-400"
-            href="https://github.com/ES-Community/nsecure"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Link to the organization
-          </a>
-        </p>
-      </footer>
+      <Footer />
     </div>
   );
 }
