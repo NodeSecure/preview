07470607f41eb62bed2687e28bba832ae7f9f8d1
Tony Gorez <gorez.tony@gmail.com>
2022-09-06 22:23:52 +0200

    chore: add playwright timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,7 @@
 import { PlaywrightTestConfig } from "@playwright/test";
 
+const SECOND = 1000;
+
 const config: PlaywrightTestConfig = {
   webServer: {
     command: "npm run dev",
@@ -8,6 +10,7 @@ const config: PlaywrightTestConfig = {
     reuseExistingServer: !process.env.CI,
   },
   workers: 4, 
+  timeout: 40 * SECOND
 };
 
 export default config;
