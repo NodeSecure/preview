77be732f4edabc43cb359166bc5149d5e1c24f28
Tony Gorez <gorez.tony@gmail.com>
2022-09-06 21:49:34 +0200

    chore: enable workers

--- e2e/search.test.ts
+++ e2e/search.test.ts
@@ -1,5 +1,7 @@
 import { test, expect, Page } from "@playwright/test";
 
+test.describe.configure({ mode: 'parallel' });
+
 test("it should display a page title", async ({ page }) => {
   await page.goto("/");
 
